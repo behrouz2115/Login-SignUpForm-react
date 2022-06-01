b0e817c27841e78bf97c09e00046ebd13cb8bc93
Behrouz Roameh <behrouz.rozmeh@gmail.com>
2022-06-01 16:22:47 +0900

    HTML part is finished and i tested it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import SignUp from './component/SignUp';
 
 const App = () => {
   return (
     <div>
-      <h1>test</h1>
+      <SignUp/>
     </div>
   );
 };
---
+++ src/component/SignUp.js
@@ -0,0 +1,52 @@
+import React,{useState} from 'react';
+
+const SignUp = () => {
+    const [data, setData]=useState({
+        name:"",
+        email:"",
+        password:"",
+        confirmPassword:"",
+        isAccepted:false
+    })
+    const changeHandler=event => {
+        if (event.target.name === "isAccepted"){
+            setData({...data,[event.target.name]:event.target.checked})
+        }else{
+            setData({...data,[event.target.name]:event.target.value})
+        }
+        console.log(event.target.value)
+    }
+    return (
+        <div>
+            <form>
+                <h2>SignUp</h2>
+                <div>
+                    <label>Name</label>
+                    <input type="text" name="name" value={data.name} onChange={changeHandler}/>
+                </div> 
+                <div>
+                    <label>Email</label>
+                    <input type="text" name="email" value={data.email} onChange={changeHandler}/>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" name="paaword" value={data.password} onChange={changeHandler}/>
+                </div>
+                <div>
+                    <label>Confirm Password</label>
+                    <input type="password" name="confirmPassword" value={data.confirmPassword} onChange={changeHandler}/>
+                    <div>
+                    <label>I accept terms of privacy policy</label>
+                    <input type="checkbox" name="isAccepted" value={data.isAccepted} onChange={changeHandler}/>
+                </div>
+                <div>
+                    <a href='#'>Login</a>
+                    <button type='submit'>Sign Up</button>
+                </div>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default SignUp;
